e7dbda192b0b05e78da81fc52b7a9e58f49b2d44
prashant-mahanta <prashantkumar.m16@iiits.in>
2019-04-14 18:04:29 +0530

    All parts complete

--- chatbot/chat/views.py
+++ chatbot/chat/views.py
@@ -38,32 +38,60 @@ class ChatAppView(APIView):
 				
 		elif text != None:
 			if file == None:
-				instance = Messages(text=text,user=user)
+				instance = Messages(text=text,file=None,user=user)
 			else:
-				instance = Messages(text=text, user=user)
+				instance = Messages(text=text, file=file, user=user)
 		if text == None and file == None:
 			pass
 		else:
+			print("instance file",instance.file)
+			if instance.file == None:
+				print("Hurray")
+			else:
+				print(instance.file)
+			print("instance text",instance.text)
 			instance.save()
 		print("save hogaya",text, file)
 		serialized_object = MessageSerializer(instance, many=False)
 		return Response(serialized_object.data)
 	
 	def put(self, request, format=None):
-		score = request.data.get("bot-feedback")
-		messages = Messages.objects.all()[::-1]
+		if request.data.get("feedback-or-file") == "1":
+			score = request.data.get("bot-feedback")
+			messages = Messages.objects.all()[::-1]
 
-		for message in messages:
-			if message.user == 1:
-				mid = message.id
-				break
-		instance = Messages(id=mid)
-		instance.bot_feedback = int(score)
-		serialized_object = MessageSerializer(instance, many=False)
-		return Response(serialized_object.data)
+			for message in messages:
+				if message.user == 1:
+					print(message.text, message.id)
+					mid = message.id
+					break
+			instance = Messages.objects.get(id=mid)
+			print(instance.text,mid)
+			instance.text = instance.text
+			instance.file = instance.file
+			instance.bot_feedback = int(score)
+			instance.save()
+			serialized_object = MessageSerializer(instance, many=False)
+			return Response(serialized_object.data)
+
+		else:
+			messages = Messages.objects.all()
+			files = []
+			for i in messages:
+				if i.file.name:
+					files.append(i)
+			serialized_object = MessageSerializer(files, many=False)
+			print(serialized_object)
+			return Response(serialized_object.data)
+		
 
 def main(request):
 	messages = Messages.objects.all()
 	# print(messages)
 	context={"messages": messages}
+	files = []
+	for i in messages:
+		if i.file.name:
+			files.append(i)
+	context["files"]=files
 	return render(request, 'index.html', context)
